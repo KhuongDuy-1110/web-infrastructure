5568ccb2ff1f54e9b2d34b16d4c4c61d139d28d3
KhuongDuy-1110 <khuongc3@gmail.com>
2025-10-03 21:00:53 +0700

    03102025v4

--- server-infrastructure/main.tf
+++ server-infrastructure/main.tf
@@ -35,7 +35,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = var.public_key
 }
 
 resource "aws_instance" "web_instance" {
--- server-infrastructure/variables.tf
+++ server-infrastructure/variables.tf
@@ -2,3 +2,8 @@ variable "region" {
   description = "aws region"
   default = "ap-southeast-1"
 }
+
+variable "public_key" {
+  type        = string
+  description = "SSH public key content"
+}
