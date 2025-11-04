aea8d601f8d6a48e012516e9307149b595e53fe9
khuongpd <khuongc3@gmail.com>
2025-11-04 13:20:50 +0700

    modify output tf

--- server-infrastructure/outputs.tf
+++ server-infrastructure/outputs.tf
@@ -11,11 +11,22 @@ output "server_information" {
   }
 }
 
-output "security_groups" {
+output "security_groups_ssh" {
   description = "Security group detail information."
   value = {
-    name = "EC2 Security Groups"
-    services_id = aws_instance.web_instance.vpc_security_group_ids
+    name = "EC2 Security Groups For SSH"
+    services_id = aws_security_group.sg_ssh.id
+    status = 1
+    created_at = local.vn_timestamp
+    updated_at = local.vn_timestamp
+  }
+}
+
+output "security_groups_http" {
+  description = "Security group detail information."
+  value = {
+    name = "EC2 Security Groups for Http"
+    services_id = aws_security_group.sg_http.id
     status = 1
     created_at = local.vn_timestamp
     updated_at = local.vn_timestamp
